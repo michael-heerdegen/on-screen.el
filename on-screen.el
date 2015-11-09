e023bf88708e6b6bf9b6cb204d67a20f02e080fd
Michael Heerdegen <michael_heerdegen@web.de>
2015-11-09 02:35:53 +0100

    bump version

--- on-screen.el
+++ on-screen.el
@@ -8,7 +8,7 @@
 ;; Created: 24 Jan 2013
 ;; Keywords: convenience
 ;; Homepage: https://github.com/michael-heerdegen/on-screen.el
-;; Version: 1.3
+;; Version: 1.3.2
 ;; Package-Requires: ((cl-lib "0"))
 
 
